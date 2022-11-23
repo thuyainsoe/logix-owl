dd013e6d3ff2f09822e6ca8387667317ae0cbec8
thuyainsoe <thuyainsoe@better.hr>
2022-11-23 21:07:52 +0630

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <h1>Hello World</h1>
+  <Background_ellipse/>
 </template>
 
 <script>
+import Background_ellipse from './components/background_ellipse/background_ellipse.vue';
 
 export default {
   name: 'App',
+  components:{
+    Background_ellipse
+}
 }
 </script>
 
-<style>
-
+<style lang="sass">
+  body
+    height: 2688px
 </style>
---
+++ src/components/background_ellipse/background_ellipse.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="top_ellipse">
+    <img src="../../assets/images/main_background/Ellipse_top.png" alt="">
+  </div>
+  <div class="right_ellipse">
+    <img src="../../assets/images/main_background/Ellipse_right.png" alt="">
+  </div>
+  <div class="right1_ellipse">
+    <img src="../../assets/images/main_background/Ellipse_right1.png" alt="">
+  </div>
+  <div class="left_ellipse">
+    <img src="../../assets/images/main_background/Ellipse_left.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass" scoped>
+    .top_ellipse
+        position: absolute
+        left: -24px
+        top: 0
+    .right_ellipse
+        position: absolute
+        top: 130px
+        height: 1059px
+        right: 0 
+    .left_ellipse
+        position: absolute
+        top: 1192px
+    .right1_ellipse
+        position: absolute
+        right: 0
+        top: 1987px
+</style>
