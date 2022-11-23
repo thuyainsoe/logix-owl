e1a16edf2c11665ebd04715a2d80113870ed586e
thuyainsoe <thuyainsoe@better.hr>
2022-11-23 20:02:50 +0630

    first commit

---
+++ src/components/Notes.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <li v-for="(note,index) in notes" :key="index">{{note}}</li>
+    </ul>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+    setup(){
+        const notes = ref([]);
+    }
+}
+</script>
+
+<style>
+
+</style>
